7aacf9f3782f5127732a24e0561e9ffd76d42e9b
bigboiiiiii66 <bigboiiiiii66@gmail.com>
2018-07-01 11:38:03 -0700

    Build 3d blocks

--- making_Blocks.py
+++ making_Blocks.py
@@ -1,5 +1,10 @@
 import mcpi.minecraft as minecraft
 import mcpi.block as block
+
 mc = minecraft.Minecraft.create()
+
 pos = mc.player.getTilePos()
-mc.setBlock(pos.x+3, pos.ye, pos.z, block.STONE.id)
+for delta in range(100):
+    mc.setBlock(pos.x + delta, pos.y, pos.z, block.STONE.id)
+    mc.setBlock(pos.x, pos.y + 2 * delta, pos.z, block.STONE.id)
+    mc.setBlock(pos.x, pos.y, pos.z + 3 * delta, block.STONE.id)
